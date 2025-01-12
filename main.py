ccaae111287dbe37c2945cfdda0c0095493b8545
kai <help.rajsarkar@gmail.com>
2025-01-12 13:47:44 +0530

    UI done

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+check_mark = "✔"
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
@@ -20,5 +21,24 @@ LONG_BREAK_MIN = 20
 
 window = Tk()
 window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+title_label = Label(text="Timer", fg=GREEN, font=(FONT_NAME, 50), bg=YELLOW)
+title_label.grid(column=1, row=0)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image= tomato_img)
+canvas.create_text(100, 130, text="00:00", font=(FONT_NAME, 35, "bold"), fill="white")
+canvas.grid(column=1, row=1)
+
+start_button = Button(text="Start", highlightthickness=0)
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset", highlightthickness=0)
+reset_button.grid(column=2, row=2)
+
+check_btn = Label(text="✔", bg=YELLOW, fg=GREEN)
+check_btn.grid(column=1, row=3)
 
 window.mainloop()
